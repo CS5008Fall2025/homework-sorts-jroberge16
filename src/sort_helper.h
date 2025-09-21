4ec6a263d0fc55a227c1cea5fa20622e4b8c43cf
jroberge16 <jwr1015@outlook.com>
2025-09-21 17:00:50 -0400

    feat: added and tested swap function in sort_helper.h

--- src/sort_helper.h
+++ src/sort_helper.h
@@ -7,9 +7,10 @@
 // Input: The 'address of' an index into an array for positions in an array.
 void swap(int *a, int *b)
 {
-    // TODO: Swap two integers in an array.
+    int temp = *a;
+   *a = *b;
+   *b = temp;
 }
-
 // Input: A pointer to an array (i.e. the array itself points to the first index)
 //        The size of the array (Because we do not know how big the array is automatically)
 void printIntArray(int *array, unsigned int size)
---
+++ src/test_sort_helper.c
@@ -0,0 +1,35 @@
+
+
+
+#include <stdio.h>  // Include file for standard input/output
+#include <stdlib.h> // so we can use atoi()
+#include "sort_helper.h"
+
+
+
+int test_swap(void){
+    printf("\tTesting Swap Function\n");
+    printf("\t\tTesting Basic Swap: ");
+    int a = 5;
+    int b = 10;
+    swap(&a, &b);
+
+    if (a == 10 && b == 5){
+        printf("Basic Swap Worked ✅\n");
+        return 1;
+    } else {
+        printf("Basic Swap Failed ❌\n");
+        return 0;
+    }
+
+
+
+}
+
+
+int main(void){
+    int passing = 0;
+    // swap
+    printf("🧪🧪 Running Sort Helper Tests 🧪🧪\n");
+    passing += test_swap();
+}
